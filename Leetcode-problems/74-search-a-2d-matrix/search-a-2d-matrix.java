6a013b306b1bb40ef801d7649c7ebf3899b27100
Rohaz Bhalla <158128735+Rohaz-bhalla@users.noreply.github.com>
2025-09-06 23:59:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (66.28%) - LeetSync

---
+++ Leetcode-problems/74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] a, int target) 
+    {
+        int i,j,
+        M = a.length,
+        N = a[0].length;
+
+        for( i=0; i<=M-1; i++)
+        {
+            for( j=0; j<=N-1; j++ )
+            {
+                if( a[i][j] == target )
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
