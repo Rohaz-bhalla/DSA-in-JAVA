7d36e7fb4a9780f4585f28e88b55680be8c27c68
Rohaz Bhalla <158128735+Rohaz-bhalla@users.noreply.github.com>
2025-11-06 23:40:16 +0530

    0189-ROTATE-ARRAY Stats: Time: 0 ms (100%), Space: 61.2 MB (5.14%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,10 +1,10 @@
 class Solution {
     public void rotate(int[] nums, int k) 
     {
-        k = k % nums.length;
+        k = k % nums.length; // if arr > k, we wont repeat same steps(optimize)
       reverse( nums, 0, nums.length-1 ); // reverse full array
-      reverse( nums, 0, k-1 ); // reverse upto the kth element
-      reverse( nums, k, nums.length - 1 ); // reverse the other side of arr
+      reverse( nums, 0, k-1 ); // reverse upto the first k elements
+      reverse( nums, k, nums.length - 1 ); // reverse remaining elements
     }
 
     public void reverse( int arr[], int i, int j )
